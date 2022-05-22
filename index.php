0d16b38b26bae5e2916d39900678bebb11618491
Rijad Tahiri <badvidgamer@gmail.com>
2022-05-22 08:58:33 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require "../image_gallery/app/core/app.php";
+require "../image_gallery/app/core/config.php";
+require "../image_gallery/app/core/database.php";
+require "../image_gallery/app/core/MainController.php";
+require "../image_gallery/app/core/displayMessages.php";
+define('CSS_PATH', './app/views/styles/');
+
+
+$invokeApp = new App();
